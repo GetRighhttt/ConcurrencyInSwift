5976d07cde60fd6417a2da8bc8afc16f088396d3
Stefan Bayne <stefanbayne@gmail.com>
2022-11-21 15:31:14 -0500

    First commit.

---
+++ ConcurrencyWithSwift/ConcurrencyWithSwift/ConcurrencyWithSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyWithSwiftApp.swift
+//  ConcurrencyWithSwift
+//
+//  Created by Stefan Bayne on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyWithSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcurrencyWithSwift/ConcurrencyWithSwift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ConcurrencyWithSwift
+//
+//  Created by Stefan Bayne on 11/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
